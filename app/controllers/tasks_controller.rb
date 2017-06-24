0ceb13335f86859d9e2a45aea3c2300a44473c3b
Brian Fan <kuomu.fan@yahoo.com>
2017-06-24 13:04:05 -0700

    add update/delete functionalities to task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,6 +15,29 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to list_path(@list)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to list_path(@list)
+  end
+
   private
     def task_params
       params.require(:task).permit(:description)
