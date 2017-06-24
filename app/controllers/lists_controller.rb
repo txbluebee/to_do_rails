1564a00d21b2b8454d5404932d0aef7e17d1ccb8
Brian Fan <kuomu.fan@yahoo.com>
2017-06-24 14:23:42 -0700

    add bootstrap and setup new routes

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,22 +2,20 @@ class ListsController < ApplicationController
 
   def index
     @lists = List.all
-    render :index
   end
 
   def show
     @list = List.find(params[:id])
-    render :show
   end
 
   def new
     @list = List.new
-    render :new
   end
 
   def create
     @list = List.new(list_params)
     if @list.save
+      flash[:notice] = "List successfully added!"
       redirect_to lists_path
     else
       render :new
@@ -27,7 +25,6 @@ class ListsController < ApplicationController
   ## Edit List##
   def edit
     @list = List.find(params[:id])
-    render :edit
   end
 
   def update
